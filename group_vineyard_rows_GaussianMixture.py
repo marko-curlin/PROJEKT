f4b86a57225d6298d279c6c8c6b78daacdb29e17
Marko Curlin <marko.curlin@live.com>
2022-01-18 19:19:55 +0100

    Group vineyard rows on rotated cloud

--- group_vineyard_rows_GaussianMixture.py
+++ group_vineyard_rows_GaussianMixture.py
@@ -8,7 +8,7 @@ from lib import util
 
 
 def main():
-    ply_fle_path = util.get_ply_file_path(util.SLICE_01_INLIERS_K_20_DEV_3)
+    ply_fle_path = util.get_ply_file_path(util.SLICE_01_INLIERS_K_20_DEV_3_ROTATED)
     point_cloud_data = util.read_ply_file_as_numpy_array(ply_fle_path)
 
     nr_of_groups = 6
@@ -17,7 +17,7 @@ def main():
     y = classifier.predict(point_cloud_data[:, 1].reshape(-1, 1))
 
     plt.scatter(point_cloud_data[:, 0], point_cloud_data[:, 1], c=y)
-    plt.title(f'file={basename(ply_fle_path)}\nclassifier={type(classifier)} k={nr_of_groups}')
+    plt.title(f'file={basename(ply_fle_path)}\nclassifier={type(classifier)} k={nr_of_groups}', fontsize=10)
     plt.show()
 
 
