b42987ce970562d45e043ff02fa4508e74e587d3
Marko Curlin <marko.curlin@live.com>
2022-03-19 17:39:42 +0100

    Add croatian documentation

--- group_vineyard_rows_KMeans.py
+++ group_vineyard_rows_KMeans.py
@@ -17,7 +17,7 @@ def group_vineyard_rows(point_cloud_data, nr_of_groups):
 
 
 def main():
-    ply_fle_path = util.get_ply_file_path(util.SLICE_01_INLIERS_K_20_DEV_3_ROTATED)
+    ply_fle_path = "/home/marko/PycharmProjects/PROJEKT/objects/OG-remove_noise_k=50_s=1-lin_reg_slice_dst=0.1-rotated_2D.ply"#util.get_ply_file_path(util.SLICE_01_INLIERS_K_20_DEV_3_ROTATED)
     point_cloud_data = util.read_ply_file_as_numpy_array(ply_fle_path)
 
     nr_of_groups = 6
@@ -26,10 +26,10 @@ def main():
     y = classifier.predict(point_cloud_data[:, 1].reshape(-1, 1))
 
     plt.scatter(point_cloud_data[:, 0], point_cloud_data[:, 1], c=y)
-    plt.title(f'file={basename(ply_fle_path)}\nclassifier={type(classifier)} k={nr_of_groups}', fontsize=10)
+    # plt.title(f'file={basename(ply_fle_path)}\nclassifier={type(classifier)} k={nr_of_groups}', fontsize=10)
     plt.show()
 
-    np.savetxt(util.construct_path(util.CLASSIFICATION_FOLDER, "slice_01_inliers_k=20_dev=3_k=6_rotated_y-axis.txt"),
+    np.savetxt(util.construct_path(util.CLASSIFICATION_FOLDER, "OG-remove_noise_k=50_s=1-lin_reg_slice_dst=0.1-rotated_2D.txt"),
                np.hstack((point_cloud_data[:, 0:2], y.reshape(y.shape[0], 1))))
 
 
